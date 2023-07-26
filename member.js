cd7d3bc70257b57e070730fc53818adaa6a234f4
tskrgulja <tskrgulja1@gmail.com>
2023-07-26 17:04:57 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,11 @@
+function skillsMember() {
+    var member = document.getElementById("member");
+    var skills = document.getElementById("skills");
+    var about = document.getElementById("about");
+    var contact = document.getElementById("contact");
+
+    member.style.display = "block";
+    skills.style.display = "none";
+    about.style.display = "none";
+    contact.style.display = "none";
+}
